3db7a6f924f28b4fb4d3160377b4b319ea89cf13
hwkim <ksi05503@gmail.com>
2022-09-18 17:16:48 +0900

    add log message

--- src/handlers/scheduled-event-logger.js
+++ src/handlers/scheduled-event-logger.js
@@ -5,4 +5,5 @@ exports.scheduledEventLoggerHandler = async (event, context) => {
     // All log statements are written to CloudWatch by default. For more information, see
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
     console.log(JSON.stringify(event));
+    console.log("scheduler func execute")
 };
